11a96f277ed6b4dcb2e8650b3f64c21cde3feeea
MohanLingam <114196110+MohanLingam@users.noreply.github.com>
2025-06-06 12:55:35 +0530

    Update App.js storage11

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useCallback } from "react";
 
 
 function App() {
-     const [items, setItems] = useState([]);
+     const [items, setItems] = useState(JSON.parse(localStorage.getItem('todo_list')) || []);
   
 
   useEffect (() =>{
